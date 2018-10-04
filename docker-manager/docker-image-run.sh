c0751a350f8b937e9960c4977cd61ca5b33131de
ubuntu 16.4 <rajsharmadotnet35@gmail.com>
2018-10-04 05:56:17 -0700

    test

--- docker-manager/docker-image-run.sh
+++ docker-manager/docker-image-run.sh
@@ -12,12 +12,12 @@ echo "Clearing traces from previous build"
 sudo docker stop docker-angular-app
 sudo docker rm docker-angular-app
 #sudo docker rmi $DOCKER_REGISTORY/$DOCKER_REGISTORY_ORG/$DOCKER_REGISTORY_REPO:$TAG
-sudo docker rmi $DOCKER_REGISTORY_REPO:$TAG
+sudo docker rmi $DOCKER_REGISTORY_REPO
 
 echo "Pulling the image"
 #sudo docker pull $DOCKER_REGISTORY/$DOCKER_REGISTORY_ORG/$DOCKER_REGISTORY_REPO:$TAG
-sudo docker pull $DOCKER_REGISTORY_REPO:$TAG
+sudo docker pull $DOCKER_REGISTORY_REPO
 
 echo "Running the image"
 #sudo docker run -td --name if_mpaas_onboarding_ui_development --restart unless-stopped -p 8080:8080 $DOCKER_REGISTORY/$DOCKER_REGISTORY_ORG/$DOCKER_REGISTORY_REPO:$TAG
-sudo docker run -p 3000:80 -d $DOCKER_REGISTORY_REPO:$TAG
+sudo docker run -p 3000:80 -d $DOCKER_REGISTORY_REPO
